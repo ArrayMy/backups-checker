39929468d74072ab9b397b7cef1960366ddde760
ArrayMy <34299302+ArrayMy@users.noreply.github.com>
2019-01-30 11:35:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 import configparser
+import glob
 def read_config():
     ConfigParser = configparser.RawConfigParser()   
     configFilePath = r'config'
     ConfigParser.read(configFilePath)
+    Format = ConfigParser.get('SETTINGS','Format')
     DIR = ConfigParser.get('SETTINGS','DIR')
     file = ConfigParser.get('SETTINGS','Predeclared_Files')
     file_names = 'NULL'
     if file == 'yes':
         file_names = ConfigParser.get('SETTINGS','File_names')
-        last_backup(DIR,file_names,True)
+        last_backup(DIR,file_names,Format,True)
     elif file == 'no':
-        last_backup(DIR,file_names)   
+        last_backup(DIR,file_names,Format)   
         
-def last_backup(DIR,file_names,file=False):
+def last_backup(DIR,file_names,Format,file=False):
     if file==True:
+        fnames = glob.glob("/*.txt")
         print ('dir_from_input:',DIR)
-        print (file_names)
+        print (file_names,Format)
     elif file==False:
         print ('file_name_from_cfg:',DIR)
-        print (file_names)
+        print (file_names,Format)
 
 if __name__ == '__main__':
     read_config()
