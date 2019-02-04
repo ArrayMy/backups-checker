c7a1736822c5217e6f80d08f0713e8155ba34ded
ArrayMy <34299302+ArrayMy@users.noreply.github.com>
2019-02-04 08:49:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import configparser
 import glob
+import datetime
+
+'''
+Find DATE IN NAME
+'''
+def find_date(File):
+    print(File)
+
 def read_config():
     ConfigParser = configparser.RawConfigParser()   
     configFilePath = r'config'
@@ -15,15 +23,30 @@ def read_config():
         last_backup(DIR,file_names,Format,True)
     elif file == 'no':
         last_backup(DIR,file_names,Format)
-'''
-set_date need detected Y = Year, m = Mounth, d = Day, S = sec, M = minutes, H = Hours
-'''
+
+'''set_date need detected Y = Year, m = Mounth, d = Day, S = sec, M = minutes, H = Hours'''
 def set_date(Date):
-  if Date == 'Y-m-d':
-    Date_pattern = '%{YEAR:year}-%{MONTHNUM:month}-%{MONTHDAY:day}'
-     
-    
-    
+
+  Date_pattern = ["NULL","NULL","NULL","NULL","NULL","NULL"]
+  num = 0
+  Date = Date.split("-")
+  for x in Date:
+    if x == 'Y':
+      Date_pattern[num] = '%Y'
+    elif x == 'm':
+      Date_pattern[num] = '%m'
+    elif x == 'd':
+      Date_pattern[num] = '%d'  
+    elif x == 'H':
+      Date_pattern[num] = '%H'
+    elif x == 'M':
+      Date_pattern[num] = '%M'   
+    elif x == 'S':
+      Date_pattern[num] = '%S'
+    num = num+1
+  print (Date_pattern)
+
+
 def last_backup(DIR,file_names,Format,file=False):
     if file==True:
         print ('DIR:',DIR)
@@ -32,9 +55,11 @@ def last_backup(DIR,file_names,Format,file=False):
         for x in file_names:
           for y in Format:
             if glob.glob(x+"*"+y): 
+              File = glob.glob(x+"*"+y)
+              find_date(File)
               print("Easy!")
             else:
-              print("File: "+x+"."+y+" is not exist!")
+              print("File: "+x+"(DATE)."+y+" is not exist!")
     elif file==False:
         print ('file_name_from_cfg:',DIR)
         print (file_names,Format)
